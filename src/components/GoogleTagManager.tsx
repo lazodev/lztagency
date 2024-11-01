235e4d46ff0529930287094971e4e4cda2cb96bf
Lazo Duarte <99038556+lazodev@users.noreply.github.com>
2024-11-01 20:06:03 -0300

    Update GoogleTagManager.tsx
    code snippet GTM

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -1,1 +1,19 @@
 
+import React, { useEffect } from 'react';
+
+const GoogleTagManager: React.FC<{ gtmId: string }> = ({ gtmId }) => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
+    document.head.appendChild(script);
+
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    document.body.appendChild(noscript);
+  }, [gtmId]);
+
+  return null;
+};
+
+export default GoogleTagManager;
