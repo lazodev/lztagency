272dea54f9ade8760942e37145cc92e3ce293ac3
Lazo Duarte <99038556+lazodev@users.noreply.github.com>
2024-11-01 20:07:54 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import GoogleTagManager from './GoogleTagManager';
 import reportWebVitals from './reportWebVitals';
+
+const GTM_ID = 'GTM-THKNQX4S';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GoogleTagManager gtmId={GTM_ID} />
     <App />
   </React.StrictMode>
 );
