d8346110a14ffc5dd5b80d57602484c3518bf83f
solustec <solustec.dev@gmail.com>
2024-11-28 00:08:49 -0300

    Add SSR Nextjs customer-area

--- src/pages/customer-area.tsx
+++ src/pages/customer-area.tsx
@@ -1,19 +1,43 @@
-import { NextPage } from 'next';
+import { ReactElement } from 'react';
+import DefaultLayout from '@/layouts/DefaultLayout';
+import { NextPageWithLayout } from '@/types/nextPageWithLayout';
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
-const CustomerArea: NextPage = () => {
+interface CustomerAreaProps {
+  // Adicione aqui quaisquer props que serão passadas para o componente
+  data: string; // exemplo de dados passados do servidor
+}
+
+const CustomerArea: NextPageWithLayout<CustomerAreaProps> = ({ data }) => {
   return (
     <>
       <Head>
         <title>Área de Cliente - Lazotec</title>
         <meta name="description" content="Testemunhos dos clientes da Lazotec." />
       </Head>
-      <main>
-        <h1>Área de cliente</h1>
-        <p>Descrição dos da area</p>
+      <main className="p-4">
+        <h1 className="text-3xl font-bold">Área de Cliente</h1>
+        <p>{data}</p>
       </main>
     </>
   );
+};
+
+// Função para buscar dados no servidor
+export const getServerSideProps: GetServerSideProps<CustomerAreaProps> = async () => {
+  // Realize as operações de busca de dados aqui
+  const data = "Descrição dos dados da área de cliente"; // Substitua pela lógica real de busca de dados
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+CustomerArea.getLayout = function getLayout(page: ReactElement) {
+  return <DefaultLayout>{page}</DefaultLayout>;
 };
 
 export default CustomerArea;
--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,17 +1,12 @@
 import { NextPage } from 'next';
-import Head from 'next/head';
 
 const Resources: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Resources - Lazotec</title>
-        <meta name="description" content="Recursos disponibilizados pela Lazotec." />
-      </Head>
-      <main>
+      
         <h1>Resources</h1>
         <p>Descrição dos recursos.</p>
-      </main>
+      
     </>
   );
 };
